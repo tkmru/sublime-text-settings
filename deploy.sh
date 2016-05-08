cea862b875910202c79a0a36b1fe7a2e360968b6
tkmru <i.am.tkmru@gmail.com>
2016-05-08 15:38:49 +0900

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function logging(){
+    echo "> "$*
+    $*
+}
+
+SETTINGFILES=($(ls))
+
+for FILE in ${SETTINGFILES[@]}; do
+    if [ ! -f "~/Library/Application Support/Sublime Text 3/Packages/User/$FILE" ]; then
+        logging ln -s $PWD/$FILE  "~/Library/Application Support/Sublime Text 3/Packages/User/$FILE"
+    else
+        echo $FILE already exists.
+    fi
+done
